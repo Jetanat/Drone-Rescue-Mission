e524b7f141e48b9405a8206966e409229fa7bc30
Will S <wish9643@colorado.edu>
2018-12-05 16:27:16 -0700

    updated world map

--- src/pathPlanner.py
+++ src/pathPlanner.py
@@ -30,22 +30,12 @@ GOAL=3
 #round radius up to nearest map resolution tick
 ROBOT_RADIUS=ROBOT_RADIUS+MAP_RESOLUTION-ROBOT_RADIUS%MAP_RESOLUTION
 
-class Cell:
-    def __init__(self):
-        self.status=OPEN
-
-    def get_status(self):
-        return self.status
-
-    def set_status(self, status):
-        self.status=status
-
 class WorldMap:
     def __init__(self):
-        self.map = [[Cell() for y in range(int(MAP_MAX_Y/MAP_RESOLUTION)+1)] for x in range(int(MAP_MAX_X/MAP_RESOLUTION)+1)]
+        self.map = [[0 for y in range(int(MAP_MAX_Y/MAP_RESOLUTION)+1)] for x in range(int(MAP_MAX_X/MAP_RESOLUTION)+1)]
         self.goal=None
 
-    def __world_to_map(self,x,y):
+    def _world_to_map(self,x,y):
         i = int((x-x%MAP_RESOLUTION)/MAP_RESOLUTION)
         j = int((y-y%MAP_RESOLUTION)/MAP_RESOLUTION)
         return i,j
@@ -53,47 +43,49 @@ class WorldMap:
     def dist_to_goal(x,y):
         if(self.goal == None):
             return -1
-        i,j = self.__world_to_map(x,y)
+        i,j = self._world_to_map(x,y)
         return ((i-self.goal[0])**2 + (j-self.goal[1])**2)**0.5
 
     def set_feature(self, low, high, status):
         #round input (x,y) to map resolution
         #round down min
-        min_x, min_y = self.__world_to_map(low[0],low[1])
+        min_x, min_y = self._world_to_map(low[0],low[1])
         #round up max
+        max_x, max_y = self._world_to_map(high[0], high[1])
         max_x=int((high[0]+(MAP_RESOLUTION-high[0]%MAP_RESOLUTION)+MAP_RESOLUTION)/MAP_RESOLUTION)
         max_y=int((high[1]+(MAP_RESOLUTION-high[1]%MAP_RESOLUTION)+MAP_RESOLUTION)/MAP_RESOLUTION)
         for i in range(min_x, max_x):
             for j in range(min_y, max_y):
-                self.map[i][j].set_status(status)
+                self.map[i][j] = status
         if(status==GOAL):
             self.goal=((min_x+max_x)/2, (min_y+max_y)/2)
 
-    def __inflate_cell(self, x, y):
+    def _inflate_cell(self, x, y):
         radius=int(ROBOT_RADIUS/MAP_RESOLUTION)
         for i in range(max(0, x-radius), min(int(MAP_MAX_X/MAP_RESOLUTION)+1, x+radius)):
             for j in range(max(0, y-radius), min(int(MAP_MAX_Y/MAP_RESOLUTION)+1, y+radius)):
-                if(self.map[i][j].get_status() == OPEN):
-                    self.map[i][j].set_status(INFLATION)
+                if(self.map[i][j] == OPEN):
+                    self.map[i][j] = INFLATION
 
     def inflate(self):
         for x in range(int(MAP_MAX_X/MAP_RESOLUTION)+1):
             for y in range(int(MAP_MAX_Y/MAP_RESOLUTION)+1):
-                if(self.map[x][y].get_status() == OBSTACLE):
-                    self.__inflate_cell(x,y)
+                if(self.map[x][y]  == OBSTACLE):
+                    self._inflate_cell(x,y)
 
-    def __print_obs_row(self, y):
-        print([col[y].get_status() for col in self.map])
+    def _print_obs_row(self, y):
+        print([col[y] for col in self.map])
 
     def print_obs_map(self):
         for y in range(len(self.map[0])-1, -1, -1):
-            self.__print_obs_row(y)
+            self._print_obs_row(y)
 
 def return_area_detected(data):
     # rospy.loginfo(rospy.get_caller_id() + "I heard %s", data)
     # print(data)
     # print(type(data))
     #ID : -> 0 : wall_width, 1 : wall_length, 2 : obstacle_one, 8 : obstacle_two, 9 : obstacle_three, 11 : landing
+
     dict = {0:"wall_width",1:"wall_length", 2:"obstacle_one", 8:"obstacle_two", 9:"obstacle_three",11:"landing" }
 
     global env_length, env_width
